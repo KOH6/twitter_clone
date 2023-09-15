10e8f0cd55067f126e7564522e608f540b9e5edf
KOH6 <minagk38+engineer@gmail.com>
2023-09-15 19:19:21 +0900

    ログイン後にユーザ情報をviewに表示、flashの表示

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,9 @@
 class PostsController < ApplicationController
   def index
+    if user_signed_in?
+      @user = current_user
+    else
+      @user = User.new
+    end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,12 @@ module ApplicationHelper
   def hidden_field_tag(_name, _value = nil, _options = {})
     raise 'Happiness chainではhidden_field_tagの使用を禁止しています'
   end
+
+  def convert_flash_class(key:)
+    case key
+    when 'notice' then 'success'
+    when 'error' then 'danger'
+    else key
+    end
+  end
 end
