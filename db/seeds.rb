abe951f1d7e096bac0ddbf0b209882063badbf0d
KOH6 <minagk38+engineer@gmail.com>
2023-09-18 06:26:43 +0900

    Userのseedデータ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+User.destroy_all
+
+5.times do |n|
+  user = User.new(name: "名前#{n}", user_name: "ユーザ名#{n}", email: "example#{n}@example.com",
+                  password: "#{'a' * n}111111")
+  user.skip_confirmation!
+  user.save!(context: :omniauth)
+end
