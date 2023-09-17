9bc7fd3ffe24674136d32111412a39812d7c0697
KOH6 <minagk38+engineer@gmail.com>
2023-09-18 08:38:37 +0900

    フォロワーしている人の投稿を画面に表示

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,5 +4,9 @@ class PostsController < ApplicationController
   def index
     @user = user_signed_in? ? current_user : User.new
     @posts = Post.latest
+    # 自分がフォローしている人
+    @followees = @user.followees
+    followee_ids = @followees.map(&:id)
+    @followee_posts = Post.followee_posts(followee_ids:)
   end
 end
