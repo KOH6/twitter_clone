dd06727a2feb68c0affd71ad946b958630490710
KOH6 <minagk38+engineer@gmail.com>
2023-09-18 06:52:47 +0900

    Postテーブルとseed作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,21 @@
 
 User.destroy_all
 
-5.times do |n|
-  user = User.new(name: "名前#{n}", user_name: "ユーザ名#{n}", email: "example#{n}@example.com",
-                  password: "#{'a' * n}111111")
+USER_COUNT = 5
+user_ids = []
+
+USER_COUNT.times do |n|
+  count = n + 1
+  user = User.new(name: "名前#{count}", user_name: "ユーザ名#{count}", email: "example#{count}@example.com",
+                  password: "#{'a' * count}111111")
   user.skip_confirmation!
   user.save!(context: :omniauth)
+  user_ids << user.id
+end
+
+50.times do |n|
+  post = Post.create!(
+    user_id: user_ids.sample,
+    content: "テスト投稿#{n}です。"
+  )
 end
