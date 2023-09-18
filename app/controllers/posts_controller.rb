8f197b50d1a7c47772089bbc5207fc1b57f39046
KOH6 <minagk38+engineer@gmail.com>
2023-09-18 20:04:14 +0900

    ページネーションひな形作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,10 +3,10 @@
 class PostsController < ApplicationController
   def index
     @user = user_signed_in? ? current_user : User.new
-    @posts = Post.latest
+    @posts = Post.latest.page(params[:posts_page])
     # 自分がフォローしている人
     @followees = @user.followees
     followee_ids = @followees.map(&:id)
-    @followee_posts = Post.followee_posts(followee_ids:)
+    @followee_posts = Post.followee_posts(followee_ids:).page(params[:followee_page])
   end
 end
