c7e677075fc66def8d339ca0072d6e77e1644d24
KOH6 <minagk38+engineer@gmail.com>
2023-09-18 23:02:39 +0900

    ボタンからプロフページへ遷移

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@
 Rails.application.routes.draw do
   # 開発環境でブラウザ上でメール受信できるようにする
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
-
-  resources :posts, only: %i[index]
-  resources :users, only: %i[show]
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
+
+  resources :posts, only: %i[index]
+  resources :users, only: %i[show]
 
   root 'posts#index'
 end
