8b091292ab49fb910938f00224afecca8982c4c3
KOH6 <minagk38+engineer@gmail.com>
2023-09-18 22:57:55 +0900

    プロフページのひな形作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 
   resources :posts, only: %i[index]
+  resources :users, only: %i[show]
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
