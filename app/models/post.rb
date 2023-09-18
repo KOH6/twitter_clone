7341acfe34c1b5b90bd4bd8d42a4aa8e7077b178
KOH6 <minagk38+engineer@gmail.com>
2023-09-18 21:30:14 +0900

    rubocop適用

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,11 +8,7 @@ class Post < ApplicationRecord
   scope :latest, -> { order(created_at: :desc) }
   scope :followee_posts, ->(followee_ids:) { where(user_id: followee_ids).order(created_at: :desc) }
 
-  def user_name
-    user.name
-  end
+  delegate :name, to: :user, prefix: true
 
-  def user_user_name
-    user.user_name
-  end
+  delegate :user_name, to: :user, prefix: true
 end
