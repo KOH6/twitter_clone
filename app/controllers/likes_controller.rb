41badb15ae094556ce12c7aebbd612ae830897b1
KOH6 <minagk38+engineer@gmail.com>
2023-09-24 12:11:06 +0900

    レビュー対応

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -7,7 +7,7 @@ class LikesController < ApplicationController
   end
 
   def destroy
-    @like = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
+    @like = current_user.likes.find_by(post_id: params[:post_id])
     @like.destroy
     redirect_to request.referer
   end
