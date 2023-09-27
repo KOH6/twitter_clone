318ac78d209d12ee144b13c3f5d9e7add36bd586
KOH6 <minagk38+engineer@gmail.com>
2023-09-27 18:04:44 +0900

    部屋の表示

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,12 @@
 class GroupsController < ApplicationController
+  before_action :set_user
+
   def index
+    @groups = current_user.groups
+    @other_users = @groups.map do |group|
+      group.group_members.where.not(user_id: current_user.id).first.user
+    end
+
   end
 
   def show
