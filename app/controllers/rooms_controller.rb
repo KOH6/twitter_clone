7e052cf5441de3cd006e7310773b529b6f828604
KOH6 <minagk38+engineer@gmail.com>
2023-09-28 10:37:20 +0900

    投稿のリンク押下で部屋画面に遷移

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -11,7 +11,18 @@ class RoomsController < ApplicationController
     @messages = room.messages.includes(:user).latest
   end
 
-  def create; end
+  def create
+    member_ids = [current_user.id, params[:user_id]]
+    existing_room = Room.search_existing_room(user_id: current_user.id, other_id: params[:user_id])
+    if existing_room
+      room = existing_room
+    else
+      room = Room.create
+      member_ids.each { |member_id| RoomMember.create(user_id: member_id, room_id: room.id) }
+    end
+
+    redirect_to action: :show, id: room.id
+  end
 
   private
 
