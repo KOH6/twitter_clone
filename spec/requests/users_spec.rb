1aee7651619fd8532916c2cc2ff60e374c86a78c
KOH6 <minagk38+engineer@gmail.com>
2023-10-08 11:48:08 +0900

    サインアップのrequest spec

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,11 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  let(:user) { create(:user) }
-  let(:user_params) { attributes_for(:user) }
-  let(:invalid_user_params) { attributes_for(:user, name:'') }
+  describe 'サインアップ:POST devise/registrations#create' do
+    let(:user) { create(:user) }
+    let(:user_params) { attributes_for(:user) }
+    let(:invalid_user_params) { attributes_for(:user, name:'') }
 
-  describe 'サインアップ POST devise/registrations#create' do
     context '正常系：パラメータ正常' do
       it 'requestが成功すること' do
         post user_registration_path, params: { user: user_params }
@@ -55,11 +55,46 @@ RSpec.describe 'Users', type: :request do
     end
   end
 
-  describe 'サインイン POST devise/sessions#create' do
+  describe 'ログイン:POST devise/sessions#create' do
+    let(:user) { create(:user) }
+
     context '正常系：パラメータ正常' do
+      before do
+        user.confirm
+        @user_params =  { email: user.email, password: user.password }
+      end
+
+      it 'requestが成功すること' do
+        post user_session_path, params: { user: @user_params }
+        expect(response).to have_http_status(303)
+      end
+
+      it 'ログイン状態になること' do
+        post user_session_path, params: { user: @user_params }
+        expect(controller.user_signed_in?).to be true
+      end
+
+      it 'root_pathにリダイレクトされること' do
+        post user_session_path, params: { user: @user_params }
+        expect(response).to redirect_to root_url
+      end
     end
 
     context '異常系：パラメータ不正' do
+      before do
+        user.confirm
+        @invalid_user_params =  { email: user.email, password: 'invalid' }
+      end
+
+      it 'requestが失敗すること' do
+        post user_session_path, params: { user: @invalid_user_params }
+        expect(response).to have_http_status(422)
+      end
+
+      it 'ログイン状態にならないこと' do
+        post user_session_path, params: { user: @invalid_user_params }
+        expect(controller.user_signed_in?).to be false
+      end
     end
   end
 
