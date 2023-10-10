3e8a726861d2e96d1682e84f418c1ce16bbbe73d
KOH6 <minagk38+engineer@gmail.com>
2023-10-10 21:38:22 +0900

    システムテスト、ログイン

--- spec/systems/users_spec.rb
+++ spec/systems/users_spec.rb
@@ -17,12 +17,11 @@ RSpec.describe 'Users', type: :system do
           fill_in '生年月日', with: user.birthdate
           fill_in 'パスワード', with: user.password
           fill_in 'パスワード（確認用）', with: user.password
-
           click_button '新規登録'
         end.to change(User, :count).by 1
 
-        # メールが1通送信されること
         expect(ActionMailer::Base.deliveries.size).to eq 1
+        expect(page).to have_content '本人確認用のメールを送信しました。メール内のリンクからアカウントを有効化させてください。'
       end
     end
 
@@ -34,7 +33,7 @@ RSpec.describe 'Users', type: :system do
         user2.confirm
       end
 
-      scenario 'ユーザ登録' do
+      scenario '存在するEメールでのユーザ登録' do
         expect do
           visit new_user_registration_path
           fill_in '名前', with: user.name
@@ -43,12 +42,44 @@ RSpec.describe 'Users', type: :system do
           fill_in '電話番号', with: user.phone
           fill_in '生年月日', with: user.birthdate
           fill_in 'パスワード', with: user.password
-
           click_button '新規登録'
         end.to change(User, :count).by 0
 
-        # エラーが表示されること
         expect(page).to have_content 'エラーが発生したため ユーザー は保存されませんでした。'
+        expect(page).to have_content '新規登録'
+      end
+    end
+  end
+
+  describe 'ログイン' do
+    let(:user) { create(:user) }
+
+    before do
+      user.confirm
+    end
+
+    context '正常系のとき' do
+      scenario 'ログイン' do
+        visit new_user_session_path
+        fill_in 'Eメール', with: user.email
+        fill_in 'パスワード', with: user.password
+        click_button 'ログイン'
+
+        expect(page).to have_content 'ログインしました。'
+        expect(page).to have_content user.name
+        expect(page).to have_content 'ホーム'
+      end
+    end
+
+    context '異常系のとき' do
+      scenario 'パスワード不正でログイン' do
+        visit new_user_session_path
+        fill_in 'Eメール', with: user.email
+        fill_in 'パスワード', with: ''
+        click_button 'ログイン'
+
+        expect(page).to have_content 'Eメールまたはパスワードが違います。'
+        expect(page).to have_content 'ログイン'
       end
     end
   end
